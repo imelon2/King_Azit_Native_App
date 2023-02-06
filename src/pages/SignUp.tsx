85e654b24bb434e6e6609d21d894402b99ab5941
wonhyeok.choi <yuanhe1995@naver.com>
2023-02-06 16:00:14 +0900

    wonhyeok:Redux,Navigation

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function SignUp() {
+    return(
+        <View>
+            <Text>SignUp Page</Text>
+        </View>
+    )
+}
+
+export default SignUp;
