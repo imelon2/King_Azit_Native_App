2722dfdc3b03a681fba7b22edf9a8724edbc8658
copa74 <copa74@naver.com>
2023-02-10 20:25:01 +0900

    2023-02-10 kim

--- AppInner.tsx
+++ AppInner.tsx
@@ -1,51 +1,64 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {StyleSheet, Text, View} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import SignIn from './src/pages/SignIn/SignIn';
 import SignUp from './src/pages/SignUp';
 import SignUpFinal from './src/pages/SignUpFinal';
 import { RootState } from './src/store/reducer';
+import MainPage from "./src/pages/MainPage/MainPage";
 
 
 export type RootStackParamList = {
   SignIn: undefined;
-  Main:undefined;
-  Login:undefined;
+  Main: undefined;
+  Login: undefined;
   SignUp: undefined;
   SignUpLogin: undefined;
   SignUpHome: undefined;
-  SignUpPassWord:undefined;
-  SignUpNickName:undefined;
-  SignUpCertification:undefined;
-  SignUpFinal:undefined;
+  SignUpPassWord: undefined;
+  SignUpNickName: undefined;
+  SignUpCertification: undefined;
+  SignUpFinal: undefined;
+  MainPage: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
 
 function AppInner() {
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
   console.log(isLoggedIn);
-  
+
   return (
-   <Stack.Navigator initialRouteName='SignIn' screenOptions={{
-    animation: 'slide_from_right',
-  }}>
-    <Stack.Screen
-      name="SignIn"
-      component={SignIn}
-      options={{title:'Login',headerShown:false}}
-    />
-    <Stack.Screen
-      name="SignUp"
-      component={SignUp}
-      options={{title:'SignUp',headerShown:false}}
-    />
-    <Stack.Screen
-      name="SignUpFinal"
-      component={SignUpFinal}
-      options={{title:'SignUpFinal',headerShown:false}}
-    />
-   </Stack.Navigator>
+    // <View>
+      // {!isLoggedIn ? (
+        <Stack.Navigator initialRouteName='SignIn' screenOptions={{
+          animation: 'slide_from_right',
+        }}>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ title: 'Login', headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ title: 'SignUp', headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUpFinal"
+            component={SignUpFinal}
+            options={{ title: 'SignUpFinal', headerShown: false }}
+          />
+        </Stack.Navigator>
+      // ) : (
+      //     <Tab.Navigator>
+      //       <Tab.Screen name="MainPage" component={MainPage} />
+      //     </Tab.Navigator>
+      // )}
+    // </View>
   );
 }
 
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import {createNativeStackNavigator, NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, {useCallback, useRef, useState} from 'react';
+import {  Text , View} from 'react-native';
+
+function MainPage() {
+  return (
+    <View><Text></Text></View>
+  );
+}
+
+export default MainPage;
