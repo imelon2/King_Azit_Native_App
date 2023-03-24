c454ecfe38041215d3100ffffdb3e7781bcae06f
copa74 <copa74@naver.com>
2023-03-24 19:06:30 +0900

    'game add'

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,46 +1,68 @@
-import React, { useState } from "react"
-import { Image, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View, TouchableOpacity, Animated, Dimensions } from 'react-native';
+import React, { useState, Fragment } from "react"
+import { SafeAreaView, StyleSheet, Text, View, Dimensions } from 'react-native';
 import { heightData } from '../../modules/globalStyles';
+import IconAntDesign from 'react-native-vector-icons/EvilIcons';
+import SwitchSelector from "react-native-switch-selector";
+import CardOne, { cardType } from './components/CardOne'
+import { Pattern } from "react-native-svg";
+
 const { width } = Dimensions.get("window");
 const heightScale = heightData;
-import { useSpring, animated } from "@react-spring/native";
 
-const AnimatedView = animated(View);
+
+
 
 function Game() {
-    const [activeIndex, setActiveIndex] = React.useState(1);
+    const [tab, setTab] = useState('');
 
-    function handleItemChange(nextIndex: any) {
-        setActiveIndex(nextIndex);
-    }
+    const options = [
+        { label: "BINGO", value: "bingo" },
+        { label: "BEST HAND", value: "hand" }
+    ];
+
+    const cardData : cardType[]  = [{ num: 'A', pattern: 'diamond' }, { num: 'A', pattern: 'spade' },
+    { num: 'A', pattern: 'clover' }, { num: '10', pattern: 'heart' }, { num: '10', pattern: 'spade' }];
 
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.headerStyle}>
                 <Text style={styles.fontStyle}>미니 게임</Text>
             </View>
-            <View>
-                {/* <TabBar
-                    items={["Short Tab", "Looooooooooooooooooooooong Tab", "Nah"]}
-                    activeIndex={activeIndex}
-                    onItemChange={handleItemChange}
-                    indicatorColor={colors[activeIndex]}
-                    renderItem={(item, index) => (
-                        <View
-                            style={{
-                                paddingHorizontal: 24,
-                                flex: 1,
-                                justifyContent: "center",
-                                alignItems: "center",
-                                marginBottom: 5
-                            }}
-                        >
-                            <Text>{item}</Text>
-                        </View>
-                    )}
-                /> */}
+            <View style={{ flex: 1, alignItems: 'center', paddingTop: 20 * heightScale }} >
+                <SwitchSelector
+                    style={{ width: 390 * heightScale }}
+                    options={options}
+                    initial={0}
+                    onPress={(value: string) => setTab(value)}
+                    buttonColor={'#F5FF82'}
+                    textColor={'#464646'}
+                    selectedColor={'#000'}
+                    backgroundColor={'#A8A7A7'}
+                    hasPadding
+                    height={48 * heightScale}
+                />
             </View>
-        </SafeAreaView>
+            <View style={{ flex: 2.6, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={{ flex: 1, flexDirection: 'row' }} >
+                    <Text style={styles.fontStyle2} >Kings Azit
+                    </Text>
+                    <IconAntDesign
+                        name="pencil"
+                        size={heightScale * 17}
+                        color="white"
+                    // onPress={() => navigateFunc()}
+                    />
+                </View>
+                <View style={{ flex: 3, alignItems: 'center' }} >
+                    <View style={{ flexDirection: 'row', width: 390 * heightScale }} >
+                        {cardData.map((val, key) => (
+                            <CardOne key={key} num={val.num} pattern={val.pattern} />
+                        ))}
+                    </View>
+                </View>
+            </View>
+            <View style={{ flex: 4.2 }}></View>
+        </SafeAreaView >
     )
 }
 
@@ -62,8 +84,11 @@ const styles = StyleSheet.create({
         color: 'white',
         paddingVertical: heightScale * 4.5,
     },
-
+    fontStyle2: {
+        color: 'white',
+        fontSize: 20 * heightScale,
+        fontWeight: '600',
+    },
 });
 
 export default Game
-
---
+++ src/pages/Game/components/CardOne.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react"
+import { SafeAreaView, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { heightData } from '../../../modules/globalStyles';
+import IconAntDesign from 'react-native-vector-icons/MaterialCommunityIcons';
+const { width } = Dimensions.get("window");
+const heightScale = heightData;
+
+export type cardType = {
+    num: String,
+    pattern: 'diamond' | 'heart' | 'spade' | 'clover',
+}
+
+function CardOne(props: cardType) {
+    const { num, pattern } = props
+    const cardName = { diamond: 'cards-diamond', heart: 'cards-heart', spade: 'cards-spade', clover: 'cards-club' };
+    const color = { diamond: '#EF473C', heart: '#EF473C', spade: '#373737', clover: '#373737' }
+
+    return (
+        <View style={styles.card} >
+            <View style={[styles.cardBorder, (pattern == 'diamond' || pattern == 'heart') && styles.cardBorder2]} >
+                <Text style={[{ color: color[pattern]  } , styles.fontStyle]} >A</Text>
+                <View style={{  alignItems: 'center' }} >
+                    <IconAntDesign
+                        name={cardName[pattern]}
+                        size={heightScale * 35}
+                        color={color[pattern]}
+                    // style={{ }}
+                    // onPress={() => navigateFunc()}
+                    />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'white',
+        width: 62 * heightScale,
+        height: 81 * heightScale,
+        borderRadius: 4,
+        marginRight: 20 * heightScale,
+        padding: 2 * heightScale,
+    },
+    cardBorder: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 3,
+        borderColor: '#373737',
+        borderWidth: 1,
+        // justifyContent:'center',
+       
+    },
+    cardBorder2: {
+        borderColor: '#EF473C',
+    },
+    fontStyle:{
+        fontSize: 18 * heightScale,
+        fontWeight:'600',
+        marginLeft: 5 * heightScale,
+        marginTop: 3 * heightScale,
+        // fontFamily:'Cinzel'
+    }
+});
+
+export default CardOne
