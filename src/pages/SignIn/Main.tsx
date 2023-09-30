aa72f109b20bf0f82ff8689f3fa7fa11265fc51a
copa74 <copa74@naver.com>
2023-09-30 19:11:06 +0900

    style: signin style fix

--- src/pages/SignIn/Main.tsx
+++ src/pages/SignIn/Main.tsx
@@ -1,22 +1,23 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {Image, StyleSheet, View, SafeAreaView} from 'react-native';
-import {RootStackParamList} from '../../../AppInner';
-import {heightData} from '../../modules/globalStyles';
+import {Image, View, SafeAreaView} from 'react-native';
+import {RootStackParamList} from 'AppInner';
+import {heightData} from '@/modules';
 import {BottomButton} from '@/components/Button';
+import {SigninStyles} from './SigninStyles';
 const heightScale = heightData;
 
 type MainScreenProps = NativeStackScreenProps<RootStackParamList, 'Main'>;
 
 function Main({navigation}: MainScreenProps) {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={SigninStyles.container}>
       <View style={{flex: 1.5}}></View>
-      <View style={styles.headerStyle}>
-        <Image source={require('@/assets/MainLogo.png')} style={styles.mainLogo} />
+      <View style={SigninStyles.headerStyle2}>
+        <Image source={require('@/assets/MainLogo.png')} style={SigninStyles.mainLogo} />
       </View>
       <View style={{flex: 1}}></View>
-      <View style={styles.bottonStyle}>
+      <View style={SigninStyles.bottonStyle}>
         <View style={{alignItems: 'center'}}>
           <BottomButton
             onPress={() => navigation.navigate('Login')}
@@ -38,52 +39,5 @@ function Main({navigation}: MainScreenProps) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#000',
-    flex: 1,
-  },
-  headerStyle: {
-    flex: 1.4,
-    alignItems: 'center',
-    // backgroundColor:'yellow'
-  },
-  bottonStyle: {
-    flex: 1,
-  },
-  loginButtonStyle: {
-    backgroundColor: '#F5FF82',
-    width: heightScale * 380,
-    height: heightData * 55,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: heightScale * 5,
-  },
-  buttonStyle: {
-    height: heightScale * 60,
-    width: heightScale * 390,
-    // marginHorizontal: heightScale * 29,
-    borderRadius: heightScale * 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  signUpButton: {
-    backgroundColor: '#000',
-    borderColor: '#F5FF82',
-    marginTop: heightData * 26,
-    borderWidth: 2,
-  },
-  textStyle: {
-    color: '#000000',
-    fontSize: heightScale * 16,
-    fontWeight: 'bold',
-  },
-  mainLogo: {
-    flex: 1,
-    aspectRatio: 1.5,
-    resizeMode: 'contain',
-  },
-});
 
 export default Main;
